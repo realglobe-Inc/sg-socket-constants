4ab1e635c40fd9eb5c9fbbc39906f3587bae0a8f
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-10 18:01:04 +0900

    Add auth events

---
+++ test/auth_evnets_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for authEvnets.
+ * Runs with mocha.
+ */
+'use strict'
+
+const authEvnets = require('../lib/auth_evnets.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('auth-evnets', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Auth evnets', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
